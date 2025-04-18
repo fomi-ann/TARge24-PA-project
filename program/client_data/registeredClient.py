00b264b0a70491ac1f7857d85567e64ee0a022b7
Kraandla <kevinrandla22@gmail.com>
2025-04-18 13:33:46 +0300

    Registered client subclass
    Created a subclass for Client. Defined methods.
    Init overwrites Client class variables.
     Get_client_id method returns client_id.
    Get full name method returns a list containing each part of the name separately.
    Get bank account method returns bank account.
    Get all orders method joins completed orders and pending orders lists into a singular list and returns it.
    Get completed orders returns completed orders list.
    Get pending orders method returns pending orders list.

---
+++ program/client_data/registeredClient.py
@@ -0,0 +1,57 @@
+from Client import Client
+
+
+class RegisteredClient(Client):
+    """RegisteredClient is a subclass of Client."""
+
+    def __init__(self, bank_account: str, first_name: str, last_name: str, middle_name: str = ""):
+        """Registered client constructor"""
+        self.client_id = self.create_client_id()
+        super().__init__('EE' + bank_account, first_name, last_name, middle_name)
+        self.completed_orders = []  # list of file names that contain completed orders
+        self.pending_orders = []  # list of file names that contain pending orders
+
+    def save_user(self):
+        """Saves user data into registeredClients.txt file"""
+        pass
+
+    def create_client_id(self) -> str:
+        """
+        Creates a random string of numbers 8 characters in length
+        and assigns it to the current class.
+        """
+        pass
+
+    def get_client_id(self):
+        """Get full name method which every subclass overrides."""
+        return self.client_id
+
+    def get_full_name(self) -> list:
+        """
+        Returns an ordered list containing the users first, middle and last name.
+        """
+        return [self.first_name, self.middle_name, self.last_name]
+
+    def get_bank_account(self):
+        """Returns the bank stored bank account."""
+        return self.bank_account
+
+    def set_bank_account(self):
+        """Sets the bank account of the registered user."""
+        pass
+
+    def get_order(self, order_id):
+        """Returns a specific order."""
+        pass
+
+    def get_all_orders(self) -> list:
+        """Returns a list of pending and completed orders."""
+        return self.completed_orders + self.pending_orders
+
+    def get_completed_orders(self) -> list:
+        """Returns a list of completed orders."""
+        return self.completed_orders
+
+    def get_pending_orders(self) -> list:
+        """Returns a list of pending orders."""
+        return self.pending_orders
