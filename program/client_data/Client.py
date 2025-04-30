e6f5c3de38ff271cd731f79bdce03943c49377cf
Kraandla <kevinrandla22@gmail.com>
2025-04-30 18:17:20 +0300

    Bank account EE duplicate fix
    EE string is no longer inside bank account client constructor variable to avoid duplicate EE strings.

--- program/client_data/Client.py
+++ program/client_data/Client.py
@@ -5,7 +5,7 @@ class Client(ABC):
 
     def __init__(self, bank_account: str, first_name: str, last_name: str, middle_name: str = ""):
         """Client constructor."""
-        self.bank_account = "EE" + bank_account  # EE{however many numbers a bank account has}
+        self.bank_account = bank_account
         self.first_name = first_name
         self.middle_name = middle_name
         self.last_name = last_name
